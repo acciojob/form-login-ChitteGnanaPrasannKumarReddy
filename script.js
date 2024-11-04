4c8868e5f9ce91b07ee714efa0059bc0fe1010f3
ChitteGnanaPrasannKumarReddy <116110463+ChitteGnanaPrasannKumarReddy@users.noreply.github.com>
2024-11-04 20:52:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue() { 
+	const form = document.getElementById("form1");
+	const firstName = form.elements["fname"].value;
+    const lastName = form.elements["lname"].value; 
 
+	alert(`${firstName} ${lastName}`); 
+
+	return false;
+	
+    //Write your code here
+	
 }
